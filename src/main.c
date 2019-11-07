17c9ef329593c8303eb84093a9bf3cb155728e97
Okimdone <abougadre@insea.ac.ma>
2019-11-07 02:15:23 +0100

    some structure

--- src/main.c
+++ src/main.c
@@ -10,11 +10,15 @@
 
 
 /*
+**  Small container that sets up the cashe buffer for dyn_max() -> returns the optimal f*
+*/
+int maximize_profite(Matrix r);
+/*
 **  dyn_max() takes activity + ressource + global variable Matrix matrix 
 **  and returns the optimal value for activity machine given a part of the ressource
 */
 int dyn_max(Matrix r,Matrix cache,int activity,int ressource);
-int maximize_profite(Matrix r);
+
 
 int main(int argc, char* argv[]){
     // Configuring the input file
@@ -30,41 +34,15 @@ int main(int argc, char* argv[]){
     
     // Loading the input table into the r(i, j) matrix
     Matrix r = getMatrix(file);  fclose(file);
-    
     printf("## DATA Loaded ##\n");
-    //---------------------------------------
-    
-    int optimal = maximize_profite(r);
 
-
+    // Find the optimal f* for r 
+    int optimal = maximize_profite(r);    freeMatrix(r);
     printf("Optimal value :  %d \n",optimal);
-    freeMatrix(r);
 
     return 0;
 }//~ main()
 
-int dyn_max(Matrix r,Matrix cache,int activity,int ressource){
-    if(activity==1){
-        return r->values[activity-1][ressource];
-    }
-    
-    int optimal=0;
-    for(int i=0;i<=ressource;i++){
-        int value;
-        if ( cache->values[activity-1][i] ){
-            // the value alredy calculated
-            value = cache->values[activity-1][i]; 
-        }else{
-            // calculate the value 
-            value =  r->values[activity-1][i] + dyn_max(r,cache,activity-1,ressource-i);
-        }
-        if(optimal<value){
-            optimal = value;
-        }
-    }
-    cache->values[activity-1][ressource] = optimal;
-    return optimal;
-}
 
 int maximize_profite(Matrix r){
     //A 2D array that memorized optimal values, cache[activity][ressource] = f*(activity, ressource)
@@ -74,3 +52,24 @@ int maximize_profite(Matrix r){
     return profit;
 }
 
+int dyn_max(Matrix r,Matrix cache,int activity,int ressource){
+    if(activity==1){
+        return r->values[activity-1][ressource];
+    }
+    
+    int optimal=0, value;
+    for(int i=0 ; i<=ressource ; i++) {
+        if ( cache->values[activity-1][i] ){
+            // the value already calculated
+            value = cache->values[activity-1][i]; 
+        }else{
+            // calculate the value 
+            value =  r->values[activity-1][i] + dyn_max(r,cache,activity-1,ressource-i);
+        }
+        if(optimal < value){
+            optimal = value;
+        }
+    }
+    cache->values[activity-1][ressource] = optimal;
+    return optimal;
+}
